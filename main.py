6265bcd689d60cff34709e8129510b5465bca27c
octaprog7 <goctaprog@gmail.com>
2022-08-17 15:16:59 +0500

    ...

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# micropython
+# mail: goctaprog@gmail.com
+# MIT license
 
 
-# Press the green button in the gutter to run the script.
+# Please read this before use!: https://www.vishay.com/docs/84286/veml7700.pdf
+from machine import I2C, Pin
+import veml7700vishay
+from sensor_pack.bus_service import I2cAdapter
+import time
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # пожалуйста установите выводы scl и sda в конструкторе для вашей платы, иначе ничего не заработает!
+    # please set scl and sda pins for your board, otherwise nothing will work!
+    # https://docs.micropython.org/en/latest/library/machine.I2C.html#machine-i2c
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) № для примера
+    # bus =  I2C(scl=Pin(4), sda=Pin(5), freq=100000)   # на esp8266    !
+    i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000)  # on Arduino Nano RP2040 Connect tested
+    adaptor = I2cAdapter(i2c)
+    # ps - pressure sensor
+    v7700 = veml7700vishay.Veml7700(adaptor)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # если у вас посыпались исключения, чего у меня на макетной плате с али и проводами МГТВ не наблюдается,
+    # то проверьте все соединения.
+    # Радиотехника - наука о контактах! РТФ-Чемпион!
+    sol.power(True)  # Sensor Of Lux
+    sol.set_mode(True, True)
+    curr_max = 1
+
+    for lux in sol:
+        time.sleep_ms(150)
+        curr_max = max(lux, curr_max)
+        print(f"Current illumination [lux]: {lux}\tNormalized [%]: {100 * lux / curr_max}")
--- veml7700vishay.py
+++ veml7700vishay.py
@@ -2,13 +2,20 @@
 # MIT license
 # Copyright (c) 2022 Roman Shevchik   goctaprog@gmail.com
 
+from sensor_pack import bus_service
 from sensor_pack.base_sensor import BaseSensor, Iterator
 import sys
 import ustruct
 
 
 class Veml7700(BaseSensor, Iterator):
-    """Class for work with ambient Light Sensor BH1750"""
+    """Class for work with ambient Light Sensor VEML7700"""
+
+    def __init__(self, adapter: bus_service.I2cAdapter, address: int = 0x10):
+        """  """
+        super().__init__(adapter, address)
+        # self.adapter = adapter
+
     def get_id(self):
         """No ID support in sensor!"""
         return None
