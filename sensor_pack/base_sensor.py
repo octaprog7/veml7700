a329616b72fec825508416badb3a2fec79845a84
octaprog7 <goctaprog@gmail.com>
2022-08-17 17:04:29 +0500

    add field byte_order. add method get_byte_order.

--- sensor_pack/base_sensor.py
+++ sensor_pack/base_sensor.py
@@ -7,15 +7,28 @@ from sensor_pack import bus_service
 class BaseSensor:
     """Base sensor class"""
 
-    def __init__(self, adapter: bus_service.BusAdapter, address: int):
+    def __init__(self, adapter: bus_service.BusAdapter, address: int, big_byte_order: bool):
+        """Базовый класс Датчик. если big_byte_order равен True -> порядок байтов в регистрах «big»
+        (Порядок от старшего к младшему), в противном случае порядок байтов в регистрах "little"
+        (Порядок от младшего к старшему)
+
+        Base sensor class. if big_byte_order is True -> register values byteorder is 'big'
+        else register values byteorder is 'little' """
         self.adapter = adapter
         self.address = address
+        self.byte_order = big_byte_order
 
     def get_id(self):
         raise NotImplementedError
 
     def soft_reset(self):
         raise NotImplementedError
+
+    def get_byte_order(self) -> str:
+        """Return byte order as string"""
+        if self.byte_order:
+            return "big"
+        return "little"
 
 
 class Iterator:
