d49fa882a3e70233159dcb850431a19d20557b8b
octaprog7 <goctaprog@gmail.com>
2022-08-19 14:55:36 +0500

    massive changes

--- sensor_pack/base_sensor.py
+++ sensor_pack/base_sensor.py
@@ -1,11 +1,22 @@
 # micropython
 # MIT license
 # Copyright (c) 2022 Roman Shevchik   goctaprog@gmail.com
+import micropython
+import ustruct
 from sensor_pack import bus_service
 
 
 class BaseSensor:
     """Base sensor class"""
+
+#    @staticmethod
+#    def get_sensor_byteorder(big_byte_order: bool) -> tuple:
+#        """Return byte order as tuple of strings.
+#        first element - name of byteorder of sensor ('big', 'little').
+#        second element - name of byteorder for call ustruct.unpack """
+#        if big_byte_order:
+#            return "big", ">"
+#        return "little", "<"
 
     def __init__(self, adapter: bus_service.BusAdapter, address: int, big_byte_order: bool):
         """Базовый класс Датчик. если big_byte_order равен True -> порядок байтов в регистрах «big»
@@ -16,19 +27,28 @@ class BaseSensor:
         else register values byteorder is 'little' """
         self.adapter = adapter
         self.address = address
-        self.byte_order = big_byte_order
+        self.big_byte_order = big_byte_order
+
+    def unpack(self, fmt_char: str, source: bytes) -> tuple:
+        """распаковка массива, считанного из датчика.
+        fmt_char: c, b, B, h, H, i, I, l, L, q, Q. pls see: https://docs.python.org/3/library/struct.html"""
+        if len(fmt_char) != 1:
+            raise ValueError(f"Invalid length fmt_char parameter: {len(fmt_char)}")
+        if self.is_big_byteorder():
+            bo = ">"    # 'big'
+        else:
+            bo = "<"  # 'little'
+        return ustruct.unpack(bo + fmt_char, source)
+
+    @micropython.native
+    def is_big_byteorder(self) -> bool:
+        return self.big_byte_order
 
     def get_id(self):
         raise NotImplementedError
 
     def soft_reset(self):
         raise NotImplementedError
-
-    def get_byte_order(self) -> str:
-        """Return byte order as string"""
-        if self.byte_order:
-            return "big"
-        return "little"
 
 
 class Iterator:
